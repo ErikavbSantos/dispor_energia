75dcfc7e30cd0510f59c342c36491fc75459c1ed
Spatialcaver <101186168+Spatialcaver@users.noreply.github.com>
2023-09-14 20:18:23 -0300

    atualizando home page
    criação de novos elementos (Ambientes e missões).

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
