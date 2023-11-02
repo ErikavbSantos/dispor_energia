85faccbe3a15a0f049b17471cc8d50b105b18701
Karolayne Silva <96891482+Karolayne-silva@users.noreply.github.com>
2023-11-02 16:01:41 -0300

    consumindo arquivo json e estilizando os valores do status

--- script.js
+++ script.js
@@ -73,3 +73,34 @@ inputSearch.addEventListener('input', function (){
     });
 
 })
+
+const tbody = document.querySelector('tbody');
+
+fetch("missoes.json").then((response)=> {
+    response.json().then((dados) => {
+        const data = dados.missoes;
+        data.map((missao) =>{ //ele se repete até acabar
+            const tr = document.createElement('tr')
+            tbody.appendChild(tr);
+            tr.innerHTML+= `<td> ${missao.id} </td>`;
+            tr.innerHTML+= `<td> ${missao.titulo} </td>`;
+            tr.innerHTML+= `<td> ${missao.dataSubmissao} </td>`;
+            tr.innerHTML+= `<td> <div> ${missao.status}</div> </td>`;
+            
+            const div = tr.querySelector('td div');
+            if (missao.status === "Aprovado"){
+                div.style.backgroundColor = "#84C382";
+            }
+            else if(missao.status === "Aprovado com ressalvas"){
+                div.style.backgroundColor = "#DBC86C";
+            }
+            else if(missao.status === "Rejeitado"){
+                div.style.backgroundColor = "#DD3F3F";
+            }
+            else{
+                div.style.backgroundColor = "#04473A";
+            }
+        })
+        
+    })
+})
