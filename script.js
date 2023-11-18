ceb6b504b9e2d7ceb1a15887c52fe5fab5002285
Spatialcaver <101186168+Spatialcaver@users.noreply.github.com>
2023-11-18 16:00:14 -0300

    estilizando tela de avaliador

--- script.js
+++ script.js
@@ -54,92 +54,77 @@ var enviar = document.getElementById('enviar')
 
 //PÁGINA STATUS DE MISSÃO
 
-const inputSearch = document.querySelector('#search-filter');
+async function preencherFormulario(dados) {
+  for (let campo in dados) {
+    if (dados.hasOwnProperty(campo)) {
+      const elemento = document.getElementById(campo);
 
-function searchEstilo() {
-    const data = inputSearch.value.toLowerCase();
-    const items = document.querySelector('table');
-    const trs = items.querySelectorAll('tr');
-  
-    trs.forEach((tr, index) => {
-      if (index > 0) {
-        if (window.innerWidth < 600) {
-          tr.style.display = 'flex';
-        } else {
-          tr.style.display = 'table-row';
-        }
-  
-        if (window.innerWidth < 600 && tr.textContent.toLowerCase().includes(data)) {
-          tr.style.display = 'flex';
-        } else if (window.innerWidth >= 600 && tr.textContent.toLowerCase().includes(data)) {
-          tr.style.display = 'table-row';
-        } else {
-          tr.style.display = 'none';
+      if (elemento) {
+        if (dados[campo].campo === 'inputtext') {
+          elemento.value = dados[campo].valor;
+        } else if (dados[campo].campo === 'inputimage') {
+          elemento.src = dados[campo].valor;
+        } else if (dados[campo].campo === 'radio') {
+          // Aqui, verificamos se o valor do campo é igual ao valor do radio button
+          if (elemento.value === dados[campo].valor) {
+            elemento.checked = true;
+
+            // Adicionamos uma classe ao elemento com base no valor do status
+            if (dados[campo].valor === 'Aprovado') {
+              elemento.classList.add('aprovado');
+            } else if (dados[campo].valor === 'Rejeitado') {
+              elemento.classList.add('rejeitado');
+            } else if (dados[campo].valor === 'Aprovado com ressalvas') {
+              elemento.classList.add('ressalvas');
+            }
+          }
         }
       }
+    }
+  }
+
+  const form = document.getElementById('avaliacaoForm');
+
+  if (form) {
+    // Adicionar evento para enviar o formulário quando houver uma mudança
+    form.addEventListener('change', function () {
+      window.location.href = `/${form.getAttribute('action')}?${new URLSearchParams(new FormData(form)).toString()}`;
     });
   }
-  
-inputSearch.addEventListener('input', searchEstilo);
-  
-window.addEventListener('resize', searchEstilo);
-
-searchEstilo();
- 
-
-const tbody = document.querySelector('tbody');
-
-fetch("missoes.json").then((response)=> {
-    response.json().then((dados) => {
-        const data = dados.missoes;
-        data.map((missao) =>{ //ele se repete até acabar
-            const tr = document.createElement('tr')
-            tbody.appendChild(tr);
-            tr.innerHTML+= `<td data-label="ID"> ${missao.id} </td>`;
-            tr.innerHTML+= `<td data-label="TÍTULO"> ${missao.titulo} </td>`;
-            tr.innerHTML+= `<td data-label="DATA DE SUBMISSÃO"> ${missao.dataSubmissao} </td>`;
-            tr.innerHTML+= `<td data-label="STATUS"> <div> ${missao.status}</div> </td>`;
-            
-            const div = tr.querySelector('td div');
-            if (missao.status === "Aprovado"){
-                div.style.backgroundColor = "#84C382";
-            }
-            else if(missao.status === "Aprovado com ressalvas"){
-                div.style.backgroundColor = "#DBC86C";
-            }
-            else if(missao.status === "Rejeitado"){
-                div.style.backgroundColor = "#DD3F3F";
-            }
-            else{
-                div.style.backgroundColor = "#04473A";
-            }
-        })
-        
-    })
-})
-
-// Função para desabilitar todos os checkbox, exceto um
-function desabilitarCheckboxExcetoUm(nomeDoCampo) {
-  // Obtém todos os checkbox do campo
-  const checkboxs = document.querySelectorAll(`input[name="${nomeDoCampo}"]`);
-
-  // Desabilita todos os checkbox
-  checkboxs.forEach((checkbox) => {
-    checkbox.disabled = true;
-  });
-
-  // Habilita o primeiro checkbox
-  checkboxs[0].disabled = false;
 }
 
-// Ouvinte de evento para o click de um checkbox
-document.addEventListener("click", (event) => {
-  // Verifica se o evento foi causado por um checkbox
-  if (event.target.type === "checkbox") {
-    // Obtém o nome do campo do checkbox
-    const nomeDoCampo = event.target.name;
 
-    // Chama a função para desabilitar todos os checkbox, exceto um
-    desabilitarCheckboxExcetoUm(nomeDoCampo);
-  }
+//Recebendo dados mocados e enviando para formulario do avaliador
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetch('dadosMissoes.json')
+      .then(response => response.json())
+      .then(data => preencherFormulario(data))
+      .catch(error => console.error('Erro ao carregar dados:', error));
 });
+
+async function preencherFormulario(dados) {
+  for (let campo in dados) {
+      if (dados.hasOwnProperty(campo)) {
+          const elemento = document.querySelector(`[name="${campo}"]`);
+
+          if (elemento) {
+              if (campo !== 'pontuacao') {
+                  elemento.value = dados[campo];
+              } else {
+                  // Se for o campo 'pontuacao' (icone), você pode precisar ajustar isso
+                  elemento.src = dados[campo];
+              }
+          }
+      }
+  }
+
+  const form = document.getElementById('avaliacaoForm');
+  
+  if (form) {
+      // Adicionar evento para enviar o formulário quando houver uma mudança
+      form.addEventListener('change', function() {
+          window.location.href = `/${form.getAttribute('action')}?${new URLSearchParams(new FormData(form)).toString()}`;
+      });
+  }
+}
