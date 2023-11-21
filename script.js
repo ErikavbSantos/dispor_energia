210147bffc9eed4308f8e0e6844fc5b836d475e0
Karolayne Silva <96891482+Karolayne-silva@users.noreply.github.com>
2023-11-21 09:44:31 -0300

    retornando o código js da página status de missão

--- script.js
+++ script.js
@@ -50,9 +50,76 @@ var enviar = document.getElementById('enviar')
     window.location.reload()
  }
  
-} */
+}  */
 
 //PÁGINA STATUS DE MISSÃO
+
+const inputSearch = document.querySelector('#search-filter');
+
+function searchEstilo() {
+    const data = inputSearch.value.toLowerCase();
+    const items = document.querySelector('table');
+    const trs = items.querySelectorAll('tr');
+  
+    trs.forEach((tr, index) => {
+        
+      if (index > 0) {
+        if (window.innerWidth < 600) {
+          tr.style.display = 'flex';
+        } else {
+          tr.style.display = 'table-row';
+        }
+
+        if (window.innerWidth < 600 && tr.textContent.toLowerCase().includes(data)) {
+          tr.style.display = 'flex';
+        } else if (window.innerWidth >= 600 && tr.textContent.toLowerCase().includes(data)) {
+          tr.style.display = 'table-row';
+        } else {
+          tr.style.display = 'none';
+        }
+      }
+    });
+}
+  
+inputSearch.addEventListener('input', searchEstilo);
+  
+window.addEventListener('resize', searchEstilo);
+
+searchEstilo();
+
+const tbody = document.querySelector('tbody');
+
+fetch("missoes.json").then((response)=> {
+  response.json().then((dados) => {
+      const data = dados.missoes;
+      data.map((missao) =>{ //ele se repete até acabar
+          const tr = document.createElement('tr')
+          tbody.appendChild(tr);
+          tr.innerHTML+= `<td data-label="ID"> ${missao.id} </td>`;
+          tr.innerHTML+= `<td data-label="TÍTULO"> ${missao.titulo} </td>`;
+          tr.innerHTML+= `<td data-label="DATA DE SUBMISSÃO"> ${missao.dataSubmissao} </td>`;
+          tr.innerHTML+= `<td data-label="STATUS"> <div> ${missao.status}</div> </td>`;
+            
+          const div = tr.querySelector('td div');
+          if (missao.status === "Aprovado"){
+            div.style.backgroundColor = "#84C382";
+            }
+            else if(missao.status === "Aprovado com ressalvas"){
+                div.style.backgroundColor = "#DBC86C";
+            }
+            else if(missao.status === "Rejeitado"){
+                div.style.backgroundColor = "#DD3F3F";
+            }
+            else{
+                div.style.backgroundColor = "#04473A";
+            }
+        })
+        
+    })
+})
+          
+
+
 
 async function preencherFormulario(dados) {
   for (let campo in dados) {
