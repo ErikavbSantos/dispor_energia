89f93d2bececf650f844381fd0b7f31342b03e0b
Karolayne Silva <96891482+Karolayne-silva@users.noreply.github.com>
2023-11-21 23:15:38 -0300

    criação de cards e filtro de pesquisa

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ var enviar = document.getElementById('enviar')
 
 //PÁGINA STATUS DE MISSÃO
 
-const inputSearch = document.querySelector('#search-filter');
+/* const inputSearch = document.querySelector('#search-filter');
 
 function searchEstilo() {
     const data = inputSearch.value.toLowerCase();
@@ -79,13 +79,13 @@ function searchEstilo() {
         }
       }
     });
-}
+} 
   
 inputSearch.addEventListener('input', searchEstilo);
   
 window.addEventListener('resize', searchEstilo);
 
-searchEstilo();
+searchEstilo(); 
 
 const tbody = document.querySelector('tbody');
 
@@ -117,7 +117,7 @@ fetch("missoes.json").then((response)=> {
         
     })
 })
-          
+           */
 
 
 
@@ -195,3 +195,49 @@ async function preencherFormulario(dados) {
       });
   }
 }
+
+//PÁGINA RASCUNHO
+const divrascunho = document.querySelector('.rascunho');
+
+fetch("missoes.json").then((response)=> {
+  response.json().then((dados) => {
+      const dataRascunho = dados.missoes;
+      dataRascunho.map((rascunho) => {
+
+        const ul = document.createElement('ul')
+        divrascunho.appendChild(ul);
+
+        const div = document.createElement('div');
+        ul.appendChild(div);
+        div.classList.add('ul-img');
+
+        div.innerHTML+= `<a href="teste.html"><img src='/assets/img/icone editar.svg'></a>`;
+        div.innerHTML+= `<img src='/assets/img/icone deletar.svg'>`;
+
+        ul.innerHTML+= `<li>Data: <span>${rascunho.dataSubmissao}</span></li>`;
+        ul.innerHTML+= `<li>Título: <span>${rascunho.titulo}</span></li>`;
+        ul.innerHTML+= `<li>Categoria: <span>${rascunho.categoria}</span></li>`;
+        ul.innerHTML+= `<li>Descrição: <span>${rascunho.descricao}</span></li>`;
+
+      }
+  )})
+})
+//pesquisa da página rascunho
+
+const inputSearchRascunho = document.querySelector('#filter-rascunho');
+
+function searchRascunho() {
+    const data = inputSearchRascunho.value.toLowerCase();
+    const rascunhoPesquisa = document.querySelector('.rascunho');
+    const uls = rascunhoPesquisa.querySelectorAll('ul');
+
+    uls.forEach((ul, index) => {
+        
+      if ( ul.textContent.toLowerCase().includes(data)) {
+        ul.style.display = 'grid';
+      } else {
+        ul.style.display = 'none';
+      }
+    });
+}
+inputSearchRascunho.addEventListener('input', searchRascunho);
