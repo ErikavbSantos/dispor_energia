e03fb8f9eceec6d0d3c39d1f977becbaaf1ea966
Karolayne Silva <96891482+Karolayne-silva@users.noreply.github.com>
2023-11-29 12:48:39 -0300

    criação de overlay

--- assets/js/listaMissoes.js
+++ assets/js/listaMissoes.js
@@ -8,12 +8,25 @@ axios.get('missoes.json')
       .forEach((missao => {
         const ul = document.createElement('ul');
         cards.appendChild(ul);
+
+        //criando div do overlay
+        const div = document.createElement('div');
+        ul.appendChild(div);
+        //adicionando classe na div
+        div.classList.add('overlay');
+
+        //adicionando botões na div
+        div.innerHTML += `<div class='btn-overlay'>
+          <button id="btn-detalhes">Detalhes</button>
+          <button id="btn-selecionar">Selecionar</button></div>
+      `;
   
         ul.innerHTML+= `<li>${missao.titulo}</li>`;
         ul.innerHTML+= `<li>${missao.categoria}</li>`;
         ul.innerHTML+= `<li>${missao.descricao}</li>`;
         ul.innerHTML+= `<li>${missao.dataSubmissao}</li>`;
       }))
+
 }).catch(( err ) => {
   console.log(err);
 });
