6a967446adc56764e5328bcb7fcc24f97f5b2e87
Karolayne Silva <96891482+Karolayne-silva@users.noreply.github.com>
2023-12-03 20:14:34 -0300

    mostrando os dados no form rascunho

---
+++ js/formRascunho.js
@@ -0,0 +1,54 @@
+const parametros = new URLSearchParams(window.location.search);
+
+// Obtém os valores individualmente
+const nome = parametros.get("nome");
+const categoria = parametros.get("categoria");
+const descricao = parametros.get("descricao");
+const onde = parametros.get("onde");
+const quando = parametros.get("quando");
+const como = parametros.get("como");
+const dado = parametros.get("dado");
+const frequencia = parametros.get("frequencia");
+const tempo = parametros.get("tempo");
+const oque = parametros.get("oque");
+const estagio = parametros.get("estagio");
+const pontuacao = parametros.get("pontuacao");
+
+const inputNome = document.getElementById("nome");
+inputNome.value = nome;
+const inputCategoria = document.getElementById("categoria");
+inputCategoria.value = categoria;
+const inputDescricao = document.getElementById("descricao");
+inputDescricao.value = descricao;
+
+const inputOnde = document.getElementById("onde");
+inputOnde.value = onde;
+
+const inputQuando = document.getElementById("quando");
+inputQuando.value = quando;
+const inputComo = document.getElementById("como");
+inputComo.value = como;
+const inputDado = document.getElementsByClassName("dado");
+inputDado.value = dado;
+const inputFrequencia = document.getElementsByClassName("frequencia");
+inputFrequencia.value = frequencia;
+const inputTempo = document.getElementById("tempo");
+inputTempo.value = tempo;
+const inputOque = document.getElementById("oque");
+inputOque.value = oque;
+const inputEstagio = document.getElementById("estagio");
+inputEstagio.value = estagio;
+const inputPontuacao = document.getElementById("pontuacao");
+inputPontuacao.value = pontuacao;
+
+console.log(
+  oque,
+  como,
+  onde,
+  quando,
+  dado,
+  frequencia,
+  tempo,
+  estagio,
+  pontuacao
+);
--- js/rascunho.js
+++ js/rascunho.js
@@ -1,89 +1,126 @@
-const divRascunho = document.getElementById('rascunho');
+const divRascunho = document.getElementById("rascunho");
 
-
-axios.get('missoes.json')
+axios
+  .get("missoes.json")
   .then((response) => {
     const dataRascunho = response.data.missoes;
 
     dataRascunho
       .filter((rascunho) => rascunho.rascunho === true)
       .forEach((rascunho) => {
-        const ul = document.createElement('ul');
+        const ul = document.createElement("ul");
         divRascunho.appendChild(ul);
 
-        const div = document.createElement('div');
+        const div = document.createElement("div");
         ul.appendChild(div);
-        div.classList.add('ul-img');
+        div.classList.add("ul-img");
 
         //criando img do icone deletar
-        const deleteIcon = document.createElement('img');
-        deleteIcon.src = '/assets/img/icone_deletar.svg';
-        deleteIcon.classList.add('delete-icon');
-        
+        const deleteIcon = document.createElement("img");
+        deleteIcon.src = "/assets/img/icone_deletar.svg";
+        deleteIcon.classList.add("delete-icon");
 
         //eventos do modal
-        ul.addEventListener('click', (event) => {
+        ul.addEventListener("click", (event) => {
           const clickedElement = event.target;
-        
-          if (clickedElement.classList.contains('delete-icon')) {
-              console.log('Clicou no ícone');
-              const modal = document.getElementById('modal');
-              modal.style.display = 'block';
 
+          if (clickedElement.classList.contains("delete-icon")) {
+            console.log("Clicou no ícone");
+            const modal = document.getElementById("modal");
+            modal.style.display = "block";
 
             //adicionar a lógica
 
-            document.getElementById('cancelDelete').addEventListener('click', () => {
-                console.log('clicou em cancelar')
-                modal.style.display = 'none';
-            });
+            document
+              .getElementById("cancelDelete")
+              .addEventListener("click", () => {
+                console.log("clicou em cancelar");
+                modal.style.display = "none";
+              });
 
-            document.getElementById('confirmDelete').addEventListener('click', () => {
-                console.log('clicou em deletar')
-              
-              //deleteRascunho(rascunho); // Supondo que exista uma função deleteRascunho
-                modal.style.display = 'none'; // Fecha o modal após a confirmação
-            });
+            document
+              .getElementById("confirmDelete")
+              .addEventListener("click", () => {
+                console.log("clicou em deletar");
+
+                //deleteRascunho(rascunho); // Supondo que exista uma função deleteRascunho
+                modal.style.display = "none"; // Fecha o modal após a confirmação
+              });
           }
         });
-       
 
+        const editarIcon = document.createElement("a");
+        editarIcon.classList.add("icone-editar");
+        editarIcon.href = `form_rascunho.html?nome=${encodeURIComponent(
+          rascunho.titulo
+        )}&categoria=${encodeURIComponent(
+          rascunho.categoria
+        )}&descricao=${encodeURIComponent(
+          rascunho.descricao
+        )}&pontuacao=${encodeURIComponent(
+          rascunho.pontuacao
+        )}&tempoLimite=${encodeURIComponent(
+          rascunho.tempoLimite
+        )}&dado=${encodeURIComponent(
+          rascunho.dado
+        )}&frequencia=${encodeURIComponent(
+          rascunho.frequencia
+        )}&oque=${encodeURIComponent(rascunho.oque)}&como=${encodeURIComponent(
+          rascunho.como
+        )}&quando=${encodeURIComponent(
+          rascunho.quando
+        )}&onde=${encodeURIComponent(
+          rascunho.onde
+        )}&estagio=${encodeURIComponent(rascunho.estagio)}`;
 
-        const editarIcon = document.createElement('a');
-        editarIcon.href = 'form_rascunho.html';
         editarIcon.innerHTML = `<img src='/assets/img/icone editar.svg'>`; // Corrigi o caminho da imagem
+        /* editarIcon.setAttribute("data-nome", rascunho.titulo);
+        editarIcon.setAttribute("data-categoria", rascunho.categoria);
+        editarIcon.setAttribute("data-descricao", rascunho.descricao); */
 
         div.appendChild(editarIcon);
+
+        /* ul.addEventListener("click", (event) => {
+          const clickedLink = event.target.closest("a");
+
+          if (clickedLink && clickedLink.classList.contains("icone-editar")) {
+            const elementoPai = clickedLink.closest("ul");
+            elementoPai.id = rascunho.id;
+            const nome = clickedLink.getAttribute("data-nome");
+            const categoria = clickedLink.getAttribute("data-categoria");
+            const descricao = clickedLink.getAttribute("data-descricao");
+            console.log(nome, categoria, descricao);
+            window.location.href = `form_rascunho.html?nome=${nome}&categoria=${categoria}&descricao=${descricao}`;
+          }
+        }); */
+
         div.appendChild(deleteIcon);
-        
 
         ul.innerHTML += `<li>Data: <span>${rascunho.dataSubmissao}</span></li>`;
         ul.innerHTML += `<li>Título: <span>${rascunho.titulo}</span></li>`;
         ul.innerHTML += `<li>Categoria: <span>${rascunho.categoria}</span></li>`;
         ul.innerHTML += `<li>Descrição: <span>${rascunho.descricao}</span></li>`;
-        
       });
   })
   .catch((err) => {
     console.log(err);
   });
 
-//pesquisa 
+//pesquisa
 
-const inputSearchRascunho = document.querySelector('#filter-rascunho');
+const inputSearchRascunho = document.querySelector("#filter-rascunho");
 
 function searchRascunho() {
-      const data = inputSearchRascunho.value.toLowerCase();
-      const rascunhoPesquisa = document.querySelector('.rascunho');
-      const uls = rascunhoPesquisa.querySelectorAll('ul');
+  const data = inputSearchRascunho.value.toLowerCase();
+  const rascunhoPesquisa = document.querySelector(".rascunho");
+  const uls = rascunhoPesquisa.querySelectorAll("ul");
 
-      uls.forEach((ul, index) => {
-          
-        if ( ul.textContent.toLowerCase().includes(data)) {
-          ul.style.display = 'grid';
-        } else {
-          ul.style.display = 'none';
-        }
-      });
-};
-inputSearchRascunho.addEventListener('input', searchRascunho);
+  uls.forEach((ul, index) => {
+    if (ul.textContent.toLowerCase().includes(data)) {
+      ul.style.display = "grid";
+    } else {
+      ul.style.display = "none";
+    }
+  });
+}
+inputSearchRascunho.addEventListener("input", searchRascunho);
